06ee3e7f20e43029bad374861be87d3c64bcb43a
lucca <luccacaval1970@gmail.com>
2022-05-11 17:22:13 -0300

    Primer Commit

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 document.getElementById('Fecha_2').style.display = 'none';
 document.getElementById('Fecha_3').style.display = 'none';
+document.getElementById('Grupo_2').style.display = 'none';
+
+function Mostrar_Grupo_1() {
+  document.getElementById('Grupo_1').style.display ='block';
+  document.getElementById('Grupo_2').style.display = 'none';
+}
+
+function Mostrar_Grupo_2() {
+  document.getElementById('Grupo_1').style.display ='none';
+  document.getElementById('Grupo_2').style.display = 'block';
+}
 
 function Mostrar_Fecha_1() {
   document.getElementById('Fecha_1').style.display = 'block';
