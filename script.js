862177c18755c98f582dccf1cb9a9d0bff5e471c
vfran128 <vallejosfran8@gmail.com>
2022-06-01 17:46:16 -0300

    Funcionan los grupos

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 document.getElementById('Fecha_2').style.display = 'none';
 document.getElementById('Fecha_3').style.display = 'none';
+document.getElementById('table1').hidden = true;
+document.getElementById('table2').hidden = true;
+document.getElementById('table3').hidden = true;
+document.getElementById('table4').hidden = true;
+document.getElementById('table5').hidden = true;
+document.getElementById('table6').hidden = true;
+document.getElementById('table7').hidden = true;
+document.getElementById('table8').hidden = true;
+
+function mostrar(e) {
+  document.getElementById('table1').hidden = true;
+  document.getElementById('table2').hidden = true;
+  document.getElementById('table3').hidden = true;
+  document.getElementById('table4').hidden = true;
+  document.getElementById('table5').hidden = true;
+  document.getElementById('table6').hidden = true;
+  document.getElementById('table7').hidden = true;
+  document.getElementById('table8').hidden = true;
+  console.log(e.target);
+  console.log(e.target.dataset.tableid);
+  let tableid = e.target.dataset.tableid;
+  let table = document.getElementById(tableid);
+  console.log(table);
+  table.hidden = !table.hidden;
+}
 
 function Mostrar_Fecha_1() {
   document.getElementById('Fecha_1').style.display = 'block';
