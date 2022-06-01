498a2bb78108146529c88dcf7e9e1183b1820822
lucca <luccacaval1970@gmail.com>
2022-06-01 17:51:05 -0300

    Clase 1/6

---
+++ backup.js
@@ -0,0 +1,47 @@
+
+45 lines (41 sloc) 1.79 KB
+document.getElementById('Fecha_2').style.display = 'none';
+document.getElementById('Fecha_3').style.display = 'none';
+document.getElementById('Grupo_2').style.display = 'none';
+
+function Mostrar_Grupo_1() {
+  document.getElementById('Grupo_1').style.display ='block';
+  document.getElementById('Grupo_2').style.display = 'none';
+}
+
+function Mostrar_Grupo_2() {
+  document.getElementById('Grupo_1').style.display ='none';
+  document.getElementById('Grupo_2').style.display = 'block';
+}
+
+function Mostrar_Fecha_1() {
+  document.getElementById('Fecha_1').style.display = 'block';
+  document.getElementById('Fecha_2').style.display = 'none';
+  document.getElementById('Fecha_3').style.display = 'none';
+}
+function Mostrar_Fecha_2() {
+    document.getElementById('Fecha_1').style.display = 'none';
+    document.getElementById('Fecha_2').style.display = 'block';
+    document.getElementById('Fecha_3').style.display = 'none';
+}
+function Mostrar_Fecha_3() {
+    document.getElementById('Fecha_1').style.display = 'none';
+    document.getElementById('Fecha_2').style.display = 'none';
+    document.getElementById('Fecha_3').style.display = 'block';
+}
+
+function Mostrar_Fecha_1_G2() {
+  document.getElementById('Fecha_1_G2').style.display = 'block';
+  document.getElementById('Fecha_2_G2').style.display = 'none';
+  document.getElementById('Fecha_3_G2').style.display = 'none';
+}
+function Mostrar_Fecha_2_G2() {
+    document.getElementById('Fecha_1_G2').style.display = 'none';
+    document.getElementById('Fecha_2_G2').style.display = 'block';
+    document.getElementById('Fecha_3_G2').style.display = 'none';
+}
+function Mostrar_Fecha_3_G2() {
+    document.getElementById('Fecha_1_G2').style.display = 'none';
+    document.getElementById('Fecha_2_G2').style.display = 'none';
+    document.getElementById('Fecha_3_G2').style.display = 'block';
+}
--- script.js
+++ script.js
@@ -1,45 +1,20 @@
-document.getElementById('Fecha_2').style.display = 'none';
-document.getElementById('Fecha_3').style.display = 'none';
-document.getElementById('Grupo_2').style.display = 'none';
-
-function Mostrar_Grupo_1() {
-  document.getElementById('Grupo_1').style.display ='block';
-  document.getElementById('Grupo_2').style.display = 'none';
-}
-
-function Mostrar_Grupo_2() {
-  document.getElementById('Grupo_1').style.display ='none';
-  document.getElementById('Grupo_2').style.display = 'block';
+function show(e) {
+  console.log(e.target.dataset.group);
+  let div = document.querySelector('.' + e.target.dataset.group);
+  console.log(div);
+  div.hidden = !div.hidden;
+  let fechas1 = document.querySelectorAll('.Fecha_1');
+  let fechas2 = document.querySelectorAll('.Fecha_2');
+  let fechas3 = document.querySelectorAll('.Fecha_3');
+  for (let divs of fechas1) {
+    fechas1.hidden = true
+  }
 }
 
 function Mostrar_Fecha_1() {
-  document.getElementById('Fecha_1').style.display = 'block';
-  document.getElementById('Fecha_2').style.display = 'none';
-  document.getElementById('Fecha_3').style.display = 'none';
-}
-function Mostrar_Fecha_2() {
-    document.getElementById('Fecha_1').style.display = 'none';
-    document.getElementById('Fecha_2').style.display = 'block';
-    document.getElementById('Fecha_3').style.display = 'none';
-}
-function Mostrar_Fecha_3() {
-    document.getElementById('Fecha_1').style.display = 'none';
-    document.getElementById('Fecha_2').style.display = 'none';
-    document.getElementById('Fecha_3').style.display = 'block';
-}
-
-function Mostrar_Fecha_1_G2() {
-  document.getElementById('Fecha_1_G2').style.display = 'block';
-  document.getElementById('Fecha_2_G2').style.display = 'none';
-  document.getElementById('Fecha_3_G2').style.display = 'none';
-}
-function Mostrar_Fecha_2_G2() {
-    document.getElementById('Fecha_1_G2').style.display = 'none';
-    document.getElementById('Fecha_2_G2').style.display = 'block';
-    document.getElementById('Fecha_3_G2').style.display = 'none';
-}
-function Mostrar_Fecha_3_G2() {
-    document.getElementById('Fecha_1_G2').style.display = 'none';
-    document.getElementById('Fecha_2_G2').style.display = 'none';
-    document.getElementById('Fecha_3_G2').style.display = 'block';
+  let fechas = document.querySelectorAll('.Fecha_1');
+  for (let div  of fechas) {
+    if (document.parentElement.hidden == false){
+      fechas.hidden = false
+    }
 }
