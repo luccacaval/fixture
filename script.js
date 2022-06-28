4214ec315f628c6acdb98423130213a0cec7af48
luccacaval <102254045+luccacaval@users.noreply.github.com>
2022-06-28 01:22:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -56,38 +56,62 @@ function resultado(event) {
   event.preventDefault();
   let local = event.target.children[1].textContent;
   let visitante = event.target.children[4].textContent;
-  console.log(local);
-  console.log(visitante);
-  let golesLocal = event.target.children[2].value;
-  let golesVisitante = event.target.children[3].value;
+  let golesLocal = parseInt(event.target.children[2].value);
+  let golesVisitante = parseInt(event.target.children[3].value);
   let grupo = event.target.parentElement.parentElement.classList
   let i_local = 0;
   let i_visitante = 0;
   let tabla = document.getElementsByClassName(grupo)[0];
   for (var i = 1; i < 4; i++) {
     if (tabla.rows[i].cells[1].textContent === local) {
-      console.log(tabla.rows[i].cells[1].textContent);
       i_local = i;
-      console.log("local = " + i);
     }
   }
 
   for (var j = 1; j <= 4; j++) {
     if (tabla.rows[j].cells[1].textContent == visitante) {
-      i_visitante = j;
-      console.log("visitante = " + j);
+      i_visitante = j
     }
   }
 
 
   if (golesLocal < golesVisitante) {
-    console.log("gana " + visitante);
+    tabla.rows[i_visitante].cells[2].textContent = parseInt(tabla.rows[i_visitante].cells[2].textContent) + 3
+    tabla.rows[i_visitante].cells[3].textContent = parseInt(tabla.rows[i_visitante].cells[3].textContent) + 1
+    tabla.rows[i_visitante].cells[4].textContent = parseInt(tabla.rows[i_visitante].cells[4].textContent) + 1
+    tabla.rows[i_visitante].cells[7].textContent = parseInt(tabla.rows[i_visitante].cells[7].textContent) + golesVisitante
+    tabla.rows[i_visitante].cells[8].textContent = parseInt(tabla.rows[i_visitante].cells[8].textContent) + golesLocal
+    tabla.rows[i_local].cells[2].textContent = parseInt(tabla.rows[i_local].cells[2].textContent) + 1
+    tabla.rows[i_local].cells[3].textContent = parseInt(tabla.rows[i_local].cells[3].textContent) + 1
+    tabla.rows[i_local].cells[5].textContent = parseInt(tabla.rows[i_local].cells[6].textContent) + 1
+    tabla.rows[i_local].cells[7].textContent = parseInt(tabla.rows[i_local].cells[7].textContent) + golesLocal
+    tabla.rows[i_local].cells[8].textContent = parseInt(tabla.rows[i_local].cells[8].textContent) + golesVisitante
   }
   if (golesLocal > golesVisitante) {
-    console.log("gana " + local);
+    tabla.rows[i_local].cells[2].textContent = parseInt(tabla.rows[i_local].cells[2].textContent) + 3
+    tabla.rows[i_local].cells[3].textContent = parseInt(tabla.rows[i_local].cells[3].textContent) + 1
+    tabla.rows[i_local].cells[4].textContent = parseInt(tabla.rows[i_local].cells[4].textContent) + 1
+    tabla.rows[i_local].cells[7].textContent = parseInt(tabla.rows[i_local].cells[7].textContent) + golesLocal
+    tabla.rows[i_local].cells[8].textContent = parseInt(tabla.rows[i_local].cells[8].textContent) + golesVisitante
+    tabla.rows[i_visitante].cells[2].textContent = parseInt(tabla.rows[i_visitante].cells[2].textContent) + 1
+    tabla.rows[i_visitante].cells[3].textContent = parseInt(tabla.rows[i_visitante].cells[3].textContent) + 1
+    tabla.rows[i_visitante].cells[5].textContent = parseInt(tabla.rows[i_visitante].cells[6].textContent) + 1
+    tabla.rows[i_visitante].cells[7].textContent = parseInt(tabla.rows[i_visitante].cells[7].textContent) + golesVisitante
+    tabla.rows[i_visitante].cells[8].textContent = parseInt(tabla.rows[i_visitante].cells[8].textContent) + golesLocal
   }
   if (golesLocal == golesVisitante) {
-    console.log("Empate");
+    tabla.rows[i_visitante].cells[2].textContent = parseInt(tabla.rows[i_visitante].cells[2].textContent) + 1
+    tabla.rows[i_local].cells[2].textContent = parseInt(tabla.rows[i_local].cells[2].textContent) + 1
+    tabla.rows[i_visitante].cells[3].textContent = parseInt(tabla.rows[i_visitante].cells[3].textContent) + 1
+    tabla.rows[i_local].cells[3].textContent = parseInt(tabla.rows[i_local].cells[3].textContent) + 1
+    tabla.rows[i_visitante].cells[5].textContent = parseInt(tabla.rows[i_visitante].cells[5].textContent) + 1
+    tabla.rows[i_local].cells[5].textContent = parseInt(tabla.rows[i_local].cells[5].textContent) + 1
+    tabla.rows[i_visitante].cells[7].textContent = parseInt(tabla.rows[i_visitante].cells[7].textContent) + golesVisitante
+    tabla.rows[i_visitante].cells[8].textContent = parseInt(tabla.rows[i_visitante].cells[8].textContent) + golesLocal
+    tabla.rows[i_local].cells[7].textContent = parseInt(tabla.rows[i_local].cells[7].textContent) + golesLocal
+    tabla.rows[i_local].cells[8].textContent = parseInt(tabla.rows[i_local].cells[8].textContent) + golesVisitante
+  }
+  event.target.children[5].hidden = true
   }
 
-}
+
